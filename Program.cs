5e2bfe562890c30dfb47846c8ec20bd10ff65938
James Grieve <jamesonrgrieve@gmail.com>
2020-08-14 11:30:15 -0600

    LINQ part 1;

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace OddEvenSorter
+namespace TestApp
 {
     class Program
     {
@@ -13,140 +15,66 @@ namespace OddEvenSorter
                 Author: James Grieve
                 Last Modified: August 14, 2020
             */
-            
-
-            /*
-                variables: inputArray, oddArray, evenArray
-
-                explain purpose prompt
-                while the array is not full (10), and done is not entered
-                    while the input is not valid
-                        prompt for an integer, and add the integer to the array
-
-                for each item in the array
-                    if the item is even:
-                        add it to even array
-                    else:
-                        add it to odd 
-                        
-                sort each array (odd and even) between start and the number of elements we copied in
-                copy the elements from odd into the output
-                copy the elements from even into the output
 
 
-
-                function SortPartialArray (array, start, end) {
-                    bubble sort from start to end
-                }
-            */
-            
-            // The size we declare the array as "physical size" is the maximum number of things we can store (size of the shelf).
-            int[] inputArray = new int[10], oddArray = new int[10], evenArray = new int[10];
-            // The "logical" size is the number of things in the array that we actually care about (number of boxes on the shelf).
-            // When we output we can go from 0 to the logical size and we won't output a bunch of 0's, or data that was "deleted". 
-            int inputArrayLogical = 0, oddArrayLogical = 0, evenArrayLogical = 0;
-            
+            List<int> numbers = new List<int>(), output = new List<int>();
 
             Console.WriteLine("This application will take in some numbers, and sort them in order but with odd numbers first, then even numbers.");
 
+            PopulateInput(numbers);
 
-            // -----------
-            // Input Logic
-            // -----------
-            PopulateInput(inputArray, ref inputArrayLogical);
+            // IEnumerable is kinda like a list, sorta, sometimes.
+            IEnumerable<int> evens = numbers.Where(x => x % 2 == 0).OrderBy(x => x);
+            /*
+            (x => x % 2 == 0)
 
-            // ---------------
-            // Splitting Logic
-            // ---------------
-            SplitArrayOddEven(inputArray, ref inputArrayLogical, oddArray, ref oddArrayLogical, evenArray, ref evenArrayLogical);
+            Is a shorthand way of writing something like this:
 
-            // -------------
-            // Sorting Logic
-            // -------------
-            Array.Resize(ref oddArray, oddArrayLogical);
-            Array.Resize(ref evenArray, evenArrayLogical);
+            static bool anonymous(int x)
+            {
+                return x % 2 == 0;
+            }
+            */
 
-            Array.Sort(oddArray);
-            Array.Sort(evenArray);
+            IEnumerable<int> odds = numbers.Where(x => x % 2 == 1).OrderBy(x => x);
 
-            oddArray.CopyTo(inputArray, 0);
-            evenArray.CopyTo(inputArray, oddArrayLogical);
+            output.AddRange(odds.ToList());
+            output.AddRange(evens.ToList());
 
-            // ------
-            // Output
-            // ------
-            // Only works because we resized the arrays, otherwise we should use the logical size.
-            OutputArray(inputArray, inputArrayLogical, oddArray.Length, evenArray.Length);
-            
+            Output(output);
+
 
         } // This should be the ONLY point in the program that your program exits.
 
-        static void PopulateInput(int[] arrayToPopulate, ref int logicalSize)
+        static void PopulateInput(List<int> inputStore)
         {
             string userInput;
             do
             {
-                int userInputInt;
-
                 Console.Write("Please enter a integer, or \"done\" to output: ");
-                userInput = Console.ReadLine(); 
-                
+                userInput = Console.ReadLine();
+
                 try
                 {
-                    // If this parse fails, none of the rest of the try block will execute, meaning we won't add garbage data
-                    // to our array, and the logical size will not go up.
-                    userInputInt = int.Parse(userInput);
-
-                    // Order of the next two lines is important because logical size serves as the index of the next available spot.
-                    arrayToPopulate[logicalSize] = userInputInt;
-                    logicalSize++;
+                    inputStore.Add(int.Parse(userInput));
                 }
                 catch
                 {
-                    // If the user has entered "done", the try will still fail, but because it's valid for our program,
-                    // we don't want to tell the user they entered something bad.
                     if (userInput.ToLower().Trim() != "done")
                     {
                         Console.WriteLine("You entered invalid input, please try again.");
                     }
                 }
-            // While the array is not full (number of things we care about is less than max things)
-            // (number of boxes on shelf is less than size of shelf), and the user has not entered
-            // the sentinel value.
-            } while(logicalSize < arrayToPopulate.Length && userInput.ToLower().Trim() != "done");
+            } while (userInput.ToLower().Trim() != "done");
         }
 
-        static void SplitArrayOddEven(int[] input, ref int inputLogical, int[] odds, ref int oddsLogical, int[] evens, ref int evensLogical)
+
+        static void Output(List<int> theList)
         {
-            for (int i = 0; i < inputLogical; i++)
-            { 
-                // If the item is even.
-                if (input[i] % 2 == 0)
-                {
-                    // Add the current item that we're looking at in inputArray to evenArray.
-                    evens[evensLogical] = input[i];
-                    evensLogical++;
-
-                    // If we try to use "i" instead of a logical size, we would wind up with this:
-                    // Input: [2, 5, 7, 10]
-                    // Even: [2, 0, 0, 10]
-                    // Odd: [0, 5, 7, 0]
-                }
-                else
-                {
-                    odds[oddsLogical] = input[i];
-                    oddsLogical++;
-                }
-            }
-
-        }
-
-        static void OutputArray(int[] array, int arrayLogical, int oddCount, int evenCount)
-        {
-            Console.WriteLine($"You entered {oddCount} odd numbers and {evenCount} even numbers.");
-            for(int i = 0; i < arrayLogical; i++)
+            //Console.WriteLine($"You entered {oddCount} odd numbers and {evenCount} even numbers.");
+            for (int i = 0; i < theList.Count; i++)
             {
-                Console.WriteLine(array[i]);
+                Console.WriteLine(theList[i]);
             }
         }
     }
